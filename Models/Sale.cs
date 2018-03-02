d6da8349420e83f584daabadcc8335ec3dc0d42a
Mike Pettman <mp_palmatum@yahoo.com>
2018-03-01 18:54:46 -0600

    initial commit, models added

---
+++ Models/Sale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApp.Models
+{
+    class Sale
+    {
+        public decimal Amount 
+        { 
+            get;
+            set;
+        }
+
+        public DateTime Date { get; set; }
+
+    }
+}
